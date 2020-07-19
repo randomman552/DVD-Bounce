acbb1db537402333252ba2511055cda061789087
George Grainger <GraingerGY@cardiff.ac.uk>
2020-07-19 15:12:47 +0100

    Remove use of canvas
    With the removal of canvas, this solution is more compatile with other browsers.

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ It's the bouncing DVD logo...
 What more could you ask for?
 
 ## Compatibility
-Works With:
+Tested on and works with:
 - Chrome
 - Firefox
-
-Due to the use of canvas and canvas filters, this is not compatible with older/terrible browsers. Will need to work one a non-canvas version for older browsers.
+- Edge
+- Opera
--- main.js
+++ main.js
@@ -1,98 +1,60 @@
-//Main canvas used to draw final display
-const canvas = document.getElementById('canvas');
-const mainctx = canvas.getContext('2d');
-
-//Secondary canvas used to draw the image and add color filters
-const imageCanvas = document.getElementById('image-canvas');
-const imagectx = imageCanvas.getContext('2d');
+//Get window dimensions
+var dimensions = [ document.documentElement.clientWidth, document.documentElement.clientHeight ];
 
 //All filters in this list were generated using https://codepen.io/sosuke/pen/Pjoqqp
-var imageFilterOptions = [
-	'invert(90%) sepia(49%) saturate(5115%) hue-rotate(356deg) brightness(101%) contrast(106%)',
-	'invert(7%) sepia(99%) saturate(7407%) hue-rotate(247deg) brightness(106%) contrast(143%)',
-	'invert(24%) sepia(93%) saturate(6188%) hue-rotate(359deg) brightness(90%) contrast(126%)',
-	'invert(14%) sepia(78%) saturate(5891%) hue-rotate(311deg) brightness(125%) contrast(112%)',
-	'invert(90%) sepia(50%) saturate(992%) hue-rotate(101deg) brightness(107%) contrast(106%)',
-	'invert(73%) sepia(79%) saturate(6164%) hue-rotate(93deg) brightness(129%) contrast(118%)',
-	'invert(49%) sepia(100%) saturate(2654%) hue-rotate(6deg) brightness(110%) contrast(101%)',
-	'invert(10%) sepia(80%) saturate(6614%) hue-rotate(278deg) brightness(119%) contrast(121%)'
-];
-
-//Intialise image filter variable and select a random starting filter
-var imageFilter;
+var colors = [ 'red', 'blue', 'orange', 'lime', 'cyan', 'magenta', 'yellow' ];
+var colorIndex = Math.floor(Math.random() * (colors.length - 1) + 1);
+const imgDiv = document.getElementById('image');
+const imgDims = [ Number(imgDiv.style.width.replace('px', '')), Number(imgDiv.style.height.replace('px', '')) ];
 advanceImageFilter();
 
-//Get image from DOM
-const img = document.getElementById('image');
-
-/*
-* The reason we use two canvas is so that we can apply filters 
-to the image separately from the main display
-* Due to our use of filters, a browser without filter support will just see a black screen.
-More details here: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/filter
-*/
-var velocity = [ canvas.width / 3000, canvas.width / 3000 ];
-var position = [ 0, 0 ];
+var velocity = [ dimensions[0] / 3000, dimensions[0] / 3000 ];
+var position = [ dimensions[0] / 2, dimensions[1] / 2 ];
 
 //Set up window resize event so that canvas size matches screen size
 window.addEventListener('resize', () => {
-	//Update both canvas size
-	canvas.width = document.documentElement.clientWidth;
-	canvas.height = document.documentElement.clientHeight;
-	imageCanvas.width = document.documentElement.clientWidth;
-	imageCanvas.height = document.documentElement.clientHeight;
+	dimensions = [ document.documentElement.clientWidth, document.documentElement.clientHeight ];
 
 	//Re calculate position and velocity
-	if (position[0] + img.width / 2 >= canvas.width || position[0] - img.width / 2 < 0) {
-		position = [ canvas.width / 2, canvas.height / 2 ];
+	if (position[0] + imgDims[0] / 2 >= dimensions[0] || position[0] - imgDims[0] / 2 < 0) {
+		position = [ dimensions[0] / 2, dimensions[1] / 2 ];
 	}
-	if (position[1] + img.height / 2 >= canvas.height || position[1] - img.height / 2 < 0) {
-		position = [ canvas.width / 2, canvas.height / 2 ];
+	if (position[1] + imgDims[1] / 2 >= dimensions[1] || position[1] - imgDims[1] / 2 < 0) {
+		position = [ dimensions[0] / 2, dimensions[1] / 2 ];
 	}
-	velocity = [ canvas.width / 3000, canvas.width / 3000 ];
-
-	//Redraw the current frame
-	drawFrame();
+	velocity = [ Math.abs(velocity[0]) / velocity[0], Math.abs(velocity[1]) / velocity[1] ];
+	velocity = [ velocity[0] * (dimensions[0] / 3000), velocity[1] * (dimensions[0] / 3000) ];
 });
 window.dispatchEvent(new Event('resize'));
 
 /**
- * Function randomly selects a color filter for the image
+ * Advances to the next color
  * If the end of the list is reached it will loop around.
  */
 function advanceImageFilter() {
-	imageFilter = Math.floor(Math.random() * (imageFilterOptions.length - 1) + 1);
-}
-
-function drawFrame() {
-	//Draw image on image context first
-	imagectx.clearRect(0, 0, canvas.width, canvas.height);
-	imagectx.filter = imageFilterOptions[imageFilter];
-	imagectx.drawImage(img, position[0] - img.width / 2, position[1] - img.height / 2, img.width, img.height);
-
-	//Draw image from the first canvas onto the second
-	mainctx.beginPath();
-	mainctx.fillStyle = 'black';
-	mainctx.fillRect(0, 0, canvas.width, canvas.height);
-	mainctx.drawImage(imageCanvas, 0, 0);
+	colorIndex += 1;
+	if (colorIndex >= colors.length) {
+		colorIndex = 0;
+	}
+	imgDiv.style.color = colors[colorIndex];
 }
 
 function fixedUpdate() {
 	position = [ position[0] + velocity[0], position[1] + velocity[1] ];
 	//Check if colliding with wall on x axis, or wall on y axis
-	if (position[0] + img.width / 2 > canvas.width || position[0] - img.width / 2 < 0) {
+	if (position[0] + imgDims[0] / 2 > dimensions[0] || position[0] - imgDims[0] / 2 < 0) {
 		velocity[0] = velocity[0] * -1;
 		advanceImageFilter();
 	}
-	if (position[1] + img.height / 2 > canvas.height || position[1] - img.height / 2 < 0) {
+	if (position[1] + imgDims[1] / 2 > dimensions[1] || position[1] - imgDims[1] / 2 < 0) {
 		velocity[1] = velocity[1] * -1;
 		advanceImageFilter();
 	}
+	imgDiv.style.left = `${position[0] - imgDims[0] / 2}px`;
+	imgDiv.style.top = `${position[1] - imgDims[1] / 2}px`;
 }
 
-//Set interval for fixed update and draw frame
-const frameTime = 1000 / 60;
+//Set interval for fixed update
 const updateTime = 1;
 
-setInterval(drawFrame, frameTime);
 setInterval(fixedUpdate, updateTime);
